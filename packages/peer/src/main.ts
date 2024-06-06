f52bce25c4f58ee3b587fa5abe43541d1a3eb207
Zero <z-nerd@proton.me>
2024-06-06 13:59:53 -0400

    Trying to make peer work in onrender

--- packages/peer/src/main.ts
+++ packages/peer/src/main.ts
@@ -2,7 +2,13 @@ import { PeerServer } from "peer"
 
 const port = 3000
 
-const peerServer = PeerServer({ port })
+const peerServer = PeerServer({
+  port,
+  corsOptions: {
+    allowedHeaders: "*",
+    origin: "*",
+  },
+})
 
 peerServer.on("connection", (client: any) => {
   console.log(client.id, "connect")
